86f4da30fe110de48c887866abebf990bb7d17dd
TASYA AZZAHRA PUTRI <92432388+tasyaazzahraputri@users.noreply.github.com>
2021-11-17 18:28:55 +0700

    Add files via upload

---
+++ Percakapan_Postest1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+/*Nama	: Tasya Azzahra Putri
+Kelas	: C
+Npm		: 2117051043 */
+
+int main(){
+	
+	//string//
+	string nama1, nama2;
+	
+	//input//
+	cin >>nama1;
+	cin >> nama2;
+	cout <<"\n";
+	
+	//output//
+	cout<<"************Percakapan anda akan ditampilkan dibawah ini******************* \n";
+	cout<<nama1<<": Hallo salam kenal yaa nama aku \"Araa\",nama kamu siapa?\n" << nama2<<": Salam kenal juga nama aku \"Ejak\" \n";
+	
+	return 0;
+}
